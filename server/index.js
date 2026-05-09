2baf9d950a7077875f3e8f8c8d5548b807e8ca2b
deepanshudhir <dhirdeepanshu7@gmail.com>
2026-05-09 23:21:22 +0530

    Fixed CORS1

--- server/index.js
+++ server/index.js
@@ -28,16 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: function (origin, callback) {
-      // allow requests with no origin
-      if (!origin) return callback(null, true);
-
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: true,
     credentials: true,
   })
 );
